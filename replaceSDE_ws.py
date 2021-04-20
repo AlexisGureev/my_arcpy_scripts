91599356340b6cf83344662349ba4c4398403648
Alexis Gureev <AlexisGureev@gmail.com>
2021-04-20 18:26:37 +0300

    Update replaceSDE_ws.py

--- replaceSDE_ws.py
+++ replaceSDE_ws.py
@@ -1,8 +1,20 @@
 # coding=utf-8
+#%%
+import  arcpy
+
+lyrName=r'Тектоническая карта\TK2500.BASEA'
+wsNew=r'Database Connections\gdb2012_tk2500_user_PROD.sde'
+
+lyr=arcpy.mapping.Layer(lyrName)
+wsOld= lyr.workspacePath
+mxd = arcpy.mapping.MapDocument('current')
+mxd.findAndReplaceWorkspacePaths(wsOld,wsNew)
+
+#%%
 import  arcpy
 
 lyrName=r'Тектоничес\кая карта\TK2500.BASEA'
-wsNew=r'Database Connections\gdb2012ks2_work_TEST.sde'
+wsNew=r'Database Connections\gdb2012_tk2500_user_TEST.sde'
 
 lyr=arcpy.mapping.Layer(lyrName)
 wsOld= lyr.workspacePath
